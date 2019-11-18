6f31bf623fc4b50eaf6a42e8b8961744c09c9f85
Marco <alto.marco@gmail.com>
2019-11-18 14:56:19 +0100

    Concatenazione di due stringhe.

--- Programmazione/12nov/stringconcat.c
+++ Programmazione/12nov/stringconcat.c
@@ -3,14 +3,14 @@
 #include <string.h>
 
 char *my_strcat(char s1[], char s2[]) {
-    char *c = malloc((strlen(s1) + strlen(s2)) * sizeof(char));
+    char *c = malloc((strlen(s1) + strlen(s2)) * sizeof(char)); //sizeof è abbastanza inutile ma lo lascio.
     memset(c, 0, sizeof(c));
     memcpy(c, s1, strlen(s1) + 1); //Aggiungo 1 alla lunghezza perchè devo includere anche il terminatore.
     memcpy(&c[strlen(s1)], s2, strlen(s2) + 1); //nell'array non aggiungo 1 perchè se no termino prima la stringa
     return c;
 }
 
-void main() {
+int main() {
     char a[100];
     char b[100];
     printf("Inserisci la prima stringa!:\n");
