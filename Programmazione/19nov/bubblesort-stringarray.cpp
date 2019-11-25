f8165e85cdc535e0c10b74e87e0cdd62060fe55c
Marco <alto.marco@gmail.com>
2019-11-25 15:06:54 +0100

    esercizi vari

--- Programmazione/19nov/bubblesort-stringarray.cpp
+++ Programmazione/19nov/bubblesort-stringarray.cpp
@@ -1,20 +1,20 @@
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <cstring>
 //sorta un array di stringhe
+const int l1 = 10;
+const int l2 = 50;
 
-void bubbleSort(char *list[], int len) {
-    int temp;
+void bubbleSort(char list[l1][l2]) {
+    char temp[l2];
     int i = 0, j;
     int swapped = 1;
     while (swapped == 1) {
         swapped = 0;
-        for (j = 0; j < len - 1 - i; j++) {
-            //Entro nella condizione lunghezza-1 * lunghezza-1-i volte, nel caso peggiore. quindi, visto che il
-            //-1 e il -1-n non ci interessano, l'ordine di grandezza è lunghezza^2
-            if (strcmp(list[][],)) {
-                temp = list[j];
-                list[j] = list[j + 1];
-                list[j + 1] = temp;
+        for (j = 0; j < l1 - 1 - i; j++) {
+            if (strcmp(list[j], list[j + 1]) == 1) {
+                memcpy(temp, list[j], l2);
+                memcpy(list[j], list[j + 1], l2);
+                memcpy(list[j + 1], temp, l2);
                 swapped = 1;
             }
         }
@@ -22,10 +22,10 @@ void bubbleSort(char *list[], int len) {
     }
 }
 
-void main() {
-    char a[10][50] = {"c", "ca", "cac", "cacc", "cacca"};
-    int i;
-    bubbleSort(a, sizeof(a) / sizeof(int));
-    for (i = 0; i < (sizeof(a) / sizeof(int)); i++)
-        printf("\n%d", a[i]);
+int main() {
+    char a[l1][l2] = {"aa", "aj", "ai", "ab", "ac", "ad", "af", "ag", "ae", "ah"};
+    int i, j = 0;
+    bubbleSort(a);
+    for (i = 0; i < l1; i++)
+        printf("%s\n", a[i]);
 }
